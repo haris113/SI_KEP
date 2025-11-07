e7afbf18b561ab7c4af4575bc192f37871771af6
FerdiNurrahman <ferdiart113@gmail.com>
2025-11-08 00:18:43 +0800

    Tinggal Content Ni Dek

--- resources/views/layout_dashboard/main.blade.php
+++ resources/views/layout_dashboard/main.blade.php
@@ -19,6 +19,9 @@
     <!-- Custom styles for this template-->
     <link href="/Admin_Template/css/sb-admin-2.min.css" rel="stylesheet">
 
+    {{-- Testing CSS Agar Sidebar Tidak Gerak --}}
+    
+
 </head>
 
 <body id="page-top">
@@ -130,17 +133,17 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Logout?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    Select "Logout" below if you are ready to end your current session.
+                    Anda Yakin Ingin Logout?
                 </div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <button class="btn btn-primary" type="button" data-dismiss="modal">Batal</button>
+                    <a class="btn btn-danger" href="login.html">Logout</a>
                 </div>
             </div>
         </div>
