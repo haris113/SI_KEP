cd5ce5d8d6212a84fc07cc308de534c7b23878bb
FerdiNurrahman <ferdiart113@gmail.com>
2025-12-05 00:40:35 +0800

    view kelembagaan anggota udah fiks ya mas haris

--- resources/views/pages/Anggota/kelembagaan/kelembagaan.blade.php
+++ resources/views/pages/Anggota/kelembagaan/kelembagaan.blade.php
@@ -32,30 +32,36 @@
                     <textarea class="form-control" rows="2" readonly>{{ $item->alamat }}</textarea>
                 </div>
 
-                <div class="col-md-4 mb-3">
+                <div class="col-md-3 mb-3">
                     <label class="form-label">Tahun Berdiri</label>
                     <input type="number" class="form-control" value="{{ $item->tahun_berdiri }}" readonly>
                 </div>
 
-                <div class="col-md-4 mb-3">
+                <div class="col-md-3 mb-3">
                     <label class="form-label">Status Kelembagaan</label>
                     <input type="text" class="form-control" value="{{ $item->status_lembaga }}" readonly>
                 </div>
 
-                <div class="col-md-4 mb-3">
+                <div class="col-md-3 mb-3">
                     <label class="form-label">Nomor Registrasi</label>
                     <input type="text" class="form-control" value="{{ $item->nomer_registrasi }}" readonly>
                 </div>
 
-                <div class="col-md-4 mb-3">
+                <div class="col-md-3 mb-3">
                     <label class="form-label">Tanggal Terdaftar</label>
                     <input type="date" class="form-control" value="{{ $item->tanggal_terdaftar }}" readonly>
                 </div>
 
-                <div class="col-md-12 mb-3">
-                    <label class="form-label">Keterangan</label>
-                    <textarea class="form-control" rows="4" readonly>{{ $item->keterangan }}</textarea>
-                </div>
+               <div class="col-md-12 mb-3">
+    <label class="form-label">Keterangan</label>
+    <textarea class="form-control" readonly
+              style="resize: none; overflow:hidden; height:auto; padding-top: .375rem;"
+              oninput="this.style.height = 'auto'; this.style.height = (this.scrollHeight)+'px';">
+{{ $item->keterangan }}
+    </textarea>
+</div>
+
+
 
             </div>
 
